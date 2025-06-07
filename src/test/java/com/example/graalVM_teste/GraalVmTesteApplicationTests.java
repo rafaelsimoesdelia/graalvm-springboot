a24b23e2ffa72e90bad1e197312257fe43d92035
Rafael Simões D'Elia <rafael.delia@arphoenix.com.br>
2025-06-07 17:46:58 -0300

    first commit

---
+++ src/test/java/com/example/graalVM_teste/GraalVmTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalVM_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalVmTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
